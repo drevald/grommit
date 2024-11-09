fbb828fd80c75e9d8f0a65c279cdc8e9c90ff3e1
Denis <denis@Deniss-MacBook-Pro.local>
2024-11-09 12:45:40 +0300

    ip added

--- grommit.go
+++ grommit.go
@@ -10,15 +10,16 @@ func main() {
 	// Define flags for the directory and port
 	dir := flag.String("dir", ".", "the directory to serve files from")
 	port := flag.String("port", "8080", "the port to serve on")
+	ip := flag.String("ip", "192.168.0.148", "the IP address to serve on")
 	flag.Parse()
 
 	// Serve files from the specified directory
 	fs := http.FileServer(http.Dir(*dir))
 	http.Handle("/", fs)
 
-	// Start the server on the specified port
-	address := ":" + *port
-	fmt.Printf("Serving %s on HTTP port %s\n", *dir, *port)
+	// Start the server on the specified IP and port
+	address := *ip + ":" + *port
+	fmt.Printf("Serving %s on http://%s\n", *dir, address)
 	if err := http.ListenAndServe(address, nil); err != nil {
 		fmt.Println("Server failed:", err)
 	}
